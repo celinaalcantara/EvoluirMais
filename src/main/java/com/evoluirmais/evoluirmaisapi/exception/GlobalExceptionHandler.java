fedfe394027d75095a6636795830ddcac5fe7be0
Celina <celinaacarmo@gmail.com>
2025-11-21 23:02:26 -0300

    Liberando as rotas do Swagger

--- src/main/java/com/evoluirmais/evoluirmaisapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/evoluirmais/evoluirmaisapi/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // 1. Exceções de Validação (Retorno 400 Bad Request
